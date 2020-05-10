b9d9c85316b53636ad17d7bc633b9e3e02c80d69
mrkirby153 <mr.austinwhyte@gmail.com>
2020-05-09 19:04:00 -0700

    Use intents

--- src/main/java/com/mrkirby153/snowsgivingbot/config/BotConfig.java
+++ src/main/java/com/mrkirby153/snowsgivingbot/config/BotConfig.java
@@ -3,9 +3,11 @@ package com.mrkirby153.snowsgivingbot.config;
 import com.mrkirby153.botcore.event.EventWaiter;
 import com.mrkirby153.snowsgivingbot.services.EventService;
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,7 +31,9 @@ public class BotConfig {
     @Bean
     public JDA getJda() throws Exception {
         log.info("Starting bot");
-        JDABuilder builder = new JDABuilder(AccountType.BOT).setToken(this.token)
+        JDABuilder builder = JDABuilder
+            .create(this.token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.GUILD_EMOJIS)
+            .setMemberCachePolicy(MemberCachePolicy.NONE).disableCache(CacheFlag.ACTIVITY, CacheFlag.VOICE_STATE, CacheFlag.CLIENT_STATUS)
             .addEventListeners(springJdaShim);
         return builder.build();
     }
