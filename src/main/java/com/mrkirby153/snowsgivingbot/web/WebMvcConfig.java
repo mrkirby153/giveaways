179133aac61b8f56dfc80f63c3d63e45ef945f74
mrkirby153 <mr.austinwhyte@gmail.com>
2020-05-12 16:57:34 -0700

    Hook up spring to serve the static files

---
+++ src/main/java/com/mrkirby153/snowsgivingbot/web/WebMvcConfig.java
@@ -0,0 +1,33 @@
+package com.mrkirby153.snowsgivingbot.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Map "/"
+        registry.addViewController("/")
+            .setViewName("forward:/index.html");
+
+        // Map "/word", "/word/word", and "/word/word/word" - except for anything starting with "/api/..." or ending with
+        // a file extension like ".js" - to index.html. By doing this, the client receives and routes the url. It also
+        // allows client-side URLs to be bookmarked.
+
+        // Single directory level - no need to exclude "api"
+        registry.addViewController("/{x:[\\w\\-]+}")
+            .setViewName("forward:/index.html");
+        // Multi-level directory path, need to exclude "api" on the first part of the path
+        registry.addViewController("/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}")
+            .setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+}
