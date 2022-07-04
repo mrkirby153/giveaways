226c17aafc93f4d10e5c80e9c2f96ee51ea7910e
mrkirby153 <mr.austinwhyte@gmail.com>
2022-07-04 13:52:48 -0700

    Handle companion objects and remove caching

--- src/main/kotlin/com/mrkirby153/giveaways/utils/Logger.kt
+++ src/main/kotlin/com/mrkirby153/giveaways/utils/Logger.kt
@@ -2,9 +2,21 @@ package com.mrkirby153.giveaways.utils
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.util.concurrent.ConcurrentHashMap
+import java.lang.reflect.Modifier
 
-val logCache = ConcurrentHashMap<Class<*>, Logger>()
+fun <T : Any> unwrapCompanionClass(clazz: Class<T>): Class<*> {
+    return clazz.enclosingClass?.let { enclosingClass ->
+        try {
+            enclosingClass.declaredFields.find { field ->
+                field.name == clazz.simpleName && Modifier.isStatic(
+                    field.modifiers
+                ) && field.type == clazz
+            }?.run { enclosingClass }
+        } catch (e: SecurityException) {
+            null
+        }
+    } ?: clazz
+}
 
-val Any.log: Logger
-    get() = logCache.computeIfAbsent(this::class.java) { LoggerFactory.getLogger(it) }
+inline val Any.log: Logger
+    get() = LoggerFactory.getLogger(unwrapCompanionClass(this::class.java))
