a0e86cbeab4841781a98b6ac3b2dfce660a0e652
mrkirby153 <mr.austinwhyte@gmail.com>
2022-07-04 16:56:19 -0700

    Properly configure quartz for clustering

--- src/main/kotlin/com/mrkirby153/giveaways/config/SpringQuartzScheduler.kt
+++ src/main/kotlin/com/mrkirby153/giveaways/config/SpringQuartzScheduler.kt
@@ -1,26 +1,23 @@
 package com.mrkirby153.giveaways.config
 
-import com.mrkirby153.giveaways.utils.log
 import org.quartz.spi.TriggerFiredBundle
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.quartz.SpringBeanJobFactory
 
 @Configuration
-class SpringQuartzScheduler(
-    private val context: ApplicationContext
-) {
+@EnableAsync
+@EnableScheduling
+class SpringQuartzScheduler {
 
     @Bean
-    fun springBeanJobFactory(): SpringBeanJobFactory {
-        val factory = AutowiringSpringBeanJobFactory()
-        log.debug("Configuring job factory")
-        factory.setApplicationContext(context)
-        return factory
-    }
+    fun jobFactory(context: ApplicationContext) =
+        AutowiringSpringBeanJobFactory().apply { setApplicationContext(context) }
 }
 
 class AutowiringSpringBeanJobFactory : SpringBeanJobFactory(), ApplicationContextAware {
